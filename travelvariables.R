721b743f80b26b013e85f4a6d0b8c9ad2002f113
Ashok <axs5498@rit.edu>
2016-08-17 00:05:15 -0400

    RPM SEM model almost done except SOC

---
+++ travelvariables.R
@@ -0,0 +1,54 @@
+#travel variables 
+
+#vist
+everyday<-sat,ti1, NA
+onceamonth<-sat,ti2, NA
+onceaweek<-sat,ti3, NA
+onceinthreeweeks<-sat,ti4, NA
+onceintwoweeks<-sat,ti5, NA
+threeormoretimesaweek<-sat,ti6, NA
+twiceaweek<-sat,ti7, NA
+leonceamonth<-sat,ti8, NA
+
+everyday<->everyday,err67, NA
+onceamonth<->onceamonth,err68, NA
+onceaweek<->onceaweek,err69, NA
+onceinthreeweeks<->onceinthreeweeks,err70, NA
+onceintwoweeks<->onceintwoweeks,err71, NA
+threeormoretimesaweek<->threeormoretimesaweek,err72, NA
+twiceaweek<->twiceaweek,err73, NA
+leonceamonth<->leonceamonth,err74, NA
+
+
+#mode
+bike<-sat,ti9, NA
+carpool<-sat,ti10, NA
+personalvehicle<-sat,ti11, NA
+publictransit<-sat,ti12, NA
+walk<-sat,ti13, NA
+
+bike<-bike,err75, NA
+carpool<-carpool,err76, NA
+personalvehicle<-personalvehicle,err77, NA
+publictransit<-publictransit,err78, NA
+walk<-walk,err79, NA
+
+
+#biwalk
+biwalk.none<-sat, ti14, NA
+biwalk.lemonth<-sat, ti15, NA
+biwalk.oncemonth<-sat, ti16, NA
+biwalk.onceweek<-sat, ti17, NA
+biwalk.oncein3weeks<-sat, ti18, NA
+biwalk.oncein2weeks<-sat, ti19, NA
+biwalk.threeormoreaweek<-sat, ti20, NA
+biwalk.twiceaweek<-sat, ti21, NA
+
+biwalk.none<->biwalk.none, err80, NA
+biwalk.lemonth<->biwalk.lemonth, err81, NA
+biwalk.oncemonth<->biwalk.oncemonth, err82, NA
+biwalk.onceweek<->biwalk.onceweek, err83, NA
+biwalk.oncein3weeks<->biwalk.oncein3weeks, err84, NA
+biwalk.oncein2weeks<->biwalk.oncein2weeks, err85, NA
+biwalk.threeormoreaweek<->biwalk.threeormoreaweek, err86, NA
+biwalk.twiceaweek<->biwalk.twiceaweek, err87, NA
